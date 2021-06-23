f3adbc806cb2b07acfd350953be2e0ba31d5a599
okorkov <alex.okorkov@gmail.com>
2021-06-23 10:44:48 -0700

    login page added

--- App.js
+++ App.js
@@ -1,21 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, Button, TextInput } from 'react-native';
+
 
 export default function App() {
+
+  const [email, onChangeEmail] = React.useState("charles@ironhorsestudio.net");
+  const [password, onChangePassword] = React.useState(null);
+  const [loggedIn, onChangeLoggedIn] = React.useState(false);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>Log in to manage Ironhorsestudio.net</Text>
+        <TextInput
+        style={styles.input}
+        onChangeText={onChangeEmail}
+        value={email}
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangePassword}
+        value={password}
+      />
+      <Button
+        title="Log In"
+        color="#f194ff"
+        onPress={() => alert('Button with adjusted color pressed')}
+      />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: 'black',
+  },input: {
+    height: 40,
+    width: '80%',
+    margin: 12,
+    borderWidth: 1,
   },
 });
